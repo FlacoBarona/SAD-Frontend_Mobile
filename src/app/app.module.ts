93280fb6a218e0a701d6a1b0dd3ad266de420201
JosephCargua <josepcargua94@gmail.com>
2024-12-11 23:15:12 -0500

    Correction of errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,19 @@ import { SupabaseClient } from '@supabase/supabase-js';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment.prod';
 
+const supabaseUrl = environment.supabaseUrl;
+const supabaseKey = environment.supabaseKey;
 @NgModule({
+  
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     {
       provide: SupabaseClient,
-      useValue: new SupabaseClient('https://urwinhkuekffjuhxgull.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyd2luaGt1ZWtmZmp1aHhndWxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2MjY3ODMsImV4cCI6MjA0NTIwMjc4M30.QaQnVhHLy4iY8PpBuuA8jDgNydwFaEwl7HmuMfHuobQ')
+      useValue: new SupabaseClient(supabaseUrl,supabaseKey )
     }
   ],
   bootstrap: [AppComponent],
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment.prod';
+
+const supabaseUrl = environment.supabaseUrl;
+const supabaseKey = environment.supabaseKey;
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +13,8 @@ export class AuthService {
 
   constructor() {
     this.supabase = createClient(
-      'https://urwinhkuekffjuhxgull.supabase.co',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyd2luaGt1ZWtmZmp1aHhndWxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2MjY3ODMsImV4cCI6MjA0NTIwMjc4M30.QaQnVhHLy4iY8PpBuuA8jDgNydwFaEwl7HmuMfHuobQ'
+      supabaseUrl,
+      supabaseKey
     );
   }
 
